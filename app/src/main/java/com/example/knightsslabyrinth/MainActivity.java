95382e5746e02a1ad9f59189c87fb87ddc2415cc
HarmanSingh <hsingh2@bu.edu>
2023-04-19 23:01:43 -0400

    actual project

---
+++ app/src/main/java/com/example/knightsslabyrinth/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.knightsslabyrinth;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import com.google.androidgamesdk.GameActivity;
+
+public class MainActivity extends GameActivity {
+    static {
+        System.loadLibrary("knightsslabyrinth");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        makeToast("onCreate()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        makeToast("onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        makeToast("onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        makeToast("onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        makeToast("onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        makeToast("onDestroy()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        makeToast("onRestart()");
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) {
+            hideSystemUi();
+        }
+    }
+
+    private void hideSystemUi() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
+    }
+
+    public void makeToast(String str) {
+        runOnUiThread(() -> Toast.makeText(this, str, Toast.LENGTH_LONG).show());
+    }
+}
