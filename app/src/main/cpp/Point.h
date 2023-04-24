55180d3d98ed33910c41ee1070bca4a452721516
nmalamug <nmalamug@bu.edu>
2023-04-23 21:32:31 -0400

    Added Point

---
+++ app/src/main/cpp/Point.h
@@ -0,0 +1,20 @@
+//
+// Created by Nicomug on 4/23/2023.
+//
+
+#ifndef KNIGHTSLABYRINTH_POINT_H
+#define KNIGHTSLABYRINTH_POINT_H
+
+
+class Point {
+public:
+    Point(double theX, double theY){
+        x = theX;
+        y = theY;
+    }
+private:
+    double x,y;
+};
+
+
+#endif //KNIGHTSLABYRINTH_POINT_H
