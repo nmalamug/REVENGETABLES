e8d8c6c4bd314e179247c3c520b59abe5fd0b1b0
Joscelynn Palen <joscie@bu.edu>
2023-04-29 17:23:49 -0400

    font tingz

--- app/src/main/java/com/example/knightslabyrinth/HighScoreFragment.java
+++ app/src/main/java/com/example/knightslabyrinth/HighScoreFragment.java
@@ -1,5 +1,7 @@
 package com.example.knightslabyrinth;
 
+import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
